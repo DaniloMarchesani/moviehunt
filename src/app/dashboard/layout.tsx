a5b83f11874b35934f537d5fef2b927a7ba9e78c
Danilo Marchesani <marchesanidanilo@gmail.com>
2024-02-16 18:53:40 +0100

    fixed UI homepage responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-[#0f0f0f] min-h-screen text-white">
+    <div className="bg-[#0f0f0f] min-h-screen text-white text-center text-wrap md:text-start">
       <nav className=" z-10 fixed md:top-0 bottom-0 left-0 md:bg-transparent bg-gray-950/90 md:h-full w-full md:w-max p-6 flex md:flex-col justify-between">
         <ul className="flex md:flex-col md:justify-evenly justify-center items-center md:gap-20 gap-16">
           <Button variant={"default"}  size={"icon"}><Link href={"/dashboard"}><Home /></Link></Button>
@@ -13,10 +13,10 @@ function layout({ children }: { children: React.ReactNode }) {
           <Button variant={"default"} size={"icon"}><Link href={"#"}><Star /></Link></Button>
           <Button variant={"default"} size={"icon"}><Link href={"#"}><Settings /></Link></Button>
         </ul>
-        <Button variant={"default"} size={"icon"}><Link href={"#"}><LogOut /></Link></Button>
+        <Button variant={"default"} size={"icon"}><Link href={"/"}><LogOut /></Link></Button>
       </nav>
       <div className="md:ml-32 p-6">
-        <h1 className="text-4xl font-bold text-white flex mb-4">
+        <h1 className="text-4xl font-bold text-white flex mb-4  ">
           Movie<span className="text-red-500 flex items-center">Hunt</span>
         </h1>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-950">
      <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-8xl font-bold text-white flex mb-4">Movie<span className="text-red-500 flex items-center">Hunt</span></h1>
-        <h3 className="text-2xl text-white">Your favourite movies, in all one place!</h3>
+        <h1 className="md:text-8xl text-6xl font-bold text-white flex mb-4">Movie<span className="text-red-500 flex items-center">Hunt</span></h1>
+        <h3 className="md:text-2xl text-xl text-white">Your favourite movies, in all one place!</h3>
      </div>
       <div className="flex gap-3 drop-shadow-md mt-10">
         <Button variant={"destructive"} size={"lg"}><Link href={"/dashboard"}>Get Started</Link></Button>
